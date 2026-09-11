641574622c0b0e973c40dfb4ac00ec6daf8f522b
m6jm4s654p-lab <m6jm4s654p@privaterelay.appleid.com>
2026-09-12 01:49:56 +0900

    Add files via upload
    v0.8.3

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='snowtech-alpine-v082';
+const CACHE='snowtech-alpine-v083';
 const ASSETS=['./','./index.html','./manifest.webmanifest','./snowtech-logo.png','./snowtech-icon-192.png','./snowtech-icon-512.png','./apple-touch-icon.png'];
 self.addEventListener('install',e=>e.waitUntil((async()=>{await caches.open(CACHE).then(c=>c.addAll(ASSETS));await self.skipWaiting();})()));
 self.addEventListener('activate',e=>e.waitUntil((async()=>{const keys=await caches.keys();await Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)));await self.clients.claim();})()));
