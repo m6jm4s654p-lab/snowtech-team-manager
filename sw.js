04820f7ab7e8f9c409584ac6e907a7dea7570fcb
m6jm4s654p-lab <m6jm4s654p@privaterelay.appleid.com>
2026-09-12 09:43:31 +0900

    Add files via upload
    v0.9.1

--- sw.js
+++ sw.js
@@ -1,5 +1,6 @@
-const CACHE='snowtech-alpine-v089';
-const ASSETS=['./','./index.html','./manifest.webmanifest','./snowtech-logo.png','./snowtech-icon-192.png','./snowtech-icon-512.png','./apple-touch-icon.png'];
+const CACHE='snowtech-alpine-v091';
+const ASSETS=['./','./index.html','./manifest.webmanifest','./snowtech-logo.png',
+  './alpine-team-manager-logo.png','./snowtech-icon-192.png','./snowtech-icon-512.png','./apple-touch-icon.png'];
 self.addEventListener('install',e=>e.waitUntil((async()=>{await caches.open(CACHE).then(c=>c.addAll(ASSETS));await self.skipWaiting();})()));
 self.addEventListener('activate',e=>e.waitUntil((async()=>{const keys=await caches.keys();await Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)));await self.clients.claim();})()));
 self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).then(resp=>{
