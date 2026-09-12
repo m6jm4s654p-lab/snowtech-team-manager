edab92244b724d3833a9adf425095537910c551a
m6jm4s654p-lab <m6jm4s654p@privaterelay.appleid.com>
2026-09-12 15:51:09 +0900

    Add files via upload
    0.13.5

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='snowtech-alpine-v0133';
+const CACHE='snowtech-alpine-v0135';
 const CACHE_PREFIX='snowtech-alpine-';
 const ASSETS=[
   './','./index.html','./manifest.webmanifest','./snowtech-logo.png',
