97311db973efa0f83683110706948be45868fbe2
m6jm4s654p-lab <m6jm4s654p@privaterelay.appleid.com>
2026-09-16 08:53:45 +0900

    Add files via upload

---
+++ README_COMPLETE_v0.13.80.txt
@@ -0,0 +1,1 @@
+Alpine Team Manager v0.13.80 COMPLETE PACKAGE
--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME='alpine-team-manager-v01379';
+const CACHE_NAME='alpine-team-manager-v01380';
 const CORE=['./','./index.html','./app.js','./manifest.webmanifest'];
 self.addEventListener('install',event=>{
   self.skipWaiting();
