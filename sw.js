6354d67dd8d82e3c96312f4e4f9ddd9ae90f8d7c
m6jm4s654p-lab <m6jm4s654p@privaterelay.appleid.com>
2026-09-16 14:22:29 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME='alpine-team-manager-v01387';
+const CACHE_NAME='alpine-team-manager-v01389';
 const CORE=['./','./index.html','./app.js','./manifest.webmanifest'];
 self.addEventListener('install',event=>{
   self.skipWaiting();
